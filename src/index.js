9eaafd93ca3f3bcc7e09d1b2384f8a807efb3490
Alex Krech <alexkrech1997@gmail.com>
2021-02-21 14:12:37 +0300

    feat: implement function "check"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    bracketsConfig = bracketsConfig.map((subArr) => subArr.join(''))
+    let splittable = true;
+
+    while (splittable) {
+        splittable = false;
+        let comparedStr
+
+        for (pair of bracketsConfig) {
+            comparedStr = str.split(pair).join('')
+            if (str.length > comparedStr.length) {
+                splittable = true;
+                str = comparedStr;
+            }
+        }
+    }
+
+    if (str.length > 0) {
+        return false
+    }
+    return true
 }
